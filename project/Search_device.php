5e611301faf16336f65cfbbd1000c82b508b37a2
jws0071 <jws0071@naver.com>
2016-11-08 17:55:32 +0900

    Search 부분 내용 수정 / user_view는 잘못 지워서 원상복귀 시킴

--- project/Search_device.php
+++ project/Search_device.php
@@ -1,1 +1,52 @@
-<!--  작성 예정 -->
+<!--  작성  중 -->
+<?php
+include "Database.php";
+
+Class User {
+		
+	private $DBObject;
+	private $connection;
+	
+	function __construct() {
+		$this->DBObject = new Database();
+		$this->connection=$this->DBObject->connectDB();
+		//echo "getuserlist connect!";
+	}
+	
+	function getUserList() {
+		
+		if($this->connection) {
+			$query = "SELECT * FROM ";
+			$statement = $this->DBObject->executeQuery($query);
+			$rows = array();
+			
+			if(count($statement)>0) {
+				//echo "getuserlist connect444!";
+				while( $row = sqlsrv_fetch_array( $statement, SQLSRV_FETCH_NUMERIC)) {
+					$rows[] = $row;
+				}
+				return $rows;
+			}
+			else return null;
+			
+			//$this->DBObject->disconnectDB();
+		}
+	}
+	
+	function getUser($nUserID) {
+	
+		if($this->connection) {
+				
+			$query = "SELECT * FROM User WHERE nUserID=".$nUserID;
+			$statement = $this->DBObject->executeQuery($query);
+				
+			if(!$statement) return $statement;
+			else return null;
+				
+			$this->DBObject->disconnectDB();
+		}
+	}
+	
+}
+	
+?>
