e138dfe3e2fdcc6fdd816600270670b3f199779e
jws0071 <jws0071@naver.com>
2016-11-09 17:35:35 +0900

    templete상에 적용 완료 /ajaxtest/test_post파일 미 사용

--- project/Search_user.php
+++ project/Search_user.php
@@ -1,78 +1,24 @@
-<?php
+<?php 
+
+
 	include "DBController.php";
-?>
-
-<div id="page-wrapper">
-
-<div class="container-fluid">
-
-<!-- Page Heading -->
-<div class="row">
-<div class="col-lg-12">
-	<h1 class="page-header">User</h1>
-</div>
-</div>
-
-<!-- /.row -->
-<div class="row">
-<div class="col-lg-12">
-<div class="panel panel-default">
-	<div class="panel-heading">
-		<h3 class="panel-title"><i class="fa fa-bar-chart-o fa-fw"></i> User List</h3>
-	</div>
-	<div class="panel-body">
-		<div class="row">
-		<div class="col-lg-6">
-   		
-    		<!-- /input-group -->
-  		</div>
-  		<!-- /.col-lg-6 -->
-  		</div>
-  		<!-- /.row -->
-  		
-  		<br />
- 		<div class="table-responsive">
-			<table id="user_list_table" class="table table-bordered table-hover table-striped">
-				<thead>
-					<tr>
-						<th>Device List</th>
-					</tr>
-				</thead>
-				<tbody>
-
-				<?php 
+	
+	
+	$sUserName1 =$_POST["name"];
+	$sUserName = ICONV("UTF-8","EUC-KR",$sUserName1);  //입력 받은 한글의 케릭터셋을 변경시킴 쿼리문에서 사용하기 위함
+	$DBControlObject = new DBController();
+	$rows = $DBControlObject->getUserDisPlayName($sUserName);  // 퀴리문 호출하여서 값을 받음
+	if(count($rows)>0) {
+		for($i=0; $i<count($rows); $i++) {  // 레코드셋을 통재로 가져오기 때문에 배열로 나타내야함
+			$device_name = ICONV("EUC-KR","UTF-8",$rows[$i][1]);
+			echo $device_name;
+			?>
+			<br>
+			<?php 
 				
-					$nUserID = $_POST["user_search_text"];
-					echo $nUserID;
-					$DBControlObject = new DBController();
-					$rows = $DBControlObject->getUserDisPlayName($nUserID);
-					if(count($rows)>0) {
-						for($i=0; $i<count($rows); $i++) {
-							$device_name = ICONV("EUC-KR","UTF-8",$rows[$i][0]);
-				?>
-					<tr>
-						<td id="user_name"><?php echo $device_name?></td>
-					</tr>
-				<?php
-		
-						}
-					}
-				?>
-				</tbody>
-			</table>
-		</div>
-		<!-- /table-responsive -->
-	</div>
-	<!-- /.panel-body -->
-</div>
-<!-- /.panel-default -->
-</div>
-<!-- /.col-lg-12 -->
-</div>
-<!-- /.row -->
+		}
+	}
 
 
-</div>
-<!-- /.container-fluid -->
-</div>
-<!-- /#page-wrapper -->
+
+?>
