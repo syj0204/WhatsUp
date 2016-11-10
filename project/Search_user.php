18f0e3644bc2cb261f7eb43fdc05c80a90e603cd
jws0071 <jws0071@naver.com>
2016-11-10 11:08:31 +0900

    UI 변경 및 디바이스 없음 표시 설정

--- project/Search_user.php
+++ project/Search_user.php
@@ -1,4 +1,7 @@
 
+
+
+
 <?php 
 
 
@@ -12,23 +15,24 @@
 	if(count($rows)>0) {
 		for($i=0; $i<count($rows); $i++) {  // 레코드셋을 통재로 가져오기 때문에 배열로 나타내야함
 			$device_name = ICONV("EUC-KR","UTF-8",$rows[$i][1]);
-
+	
 ?>
+
 <div class="col-lg-6">
        <div class="table-responsive">
-          <table class="table table-hover">        
+          <table class="table table-bordered table-hover">        
              <tbody>
-               <tr> <?php echo $device_name; ?></tr>
+               <tr > <?php echo $device_name; ?></tr>
               </tbody>
           </table>
      </div>
   </div>
-			<?php 
-				
-		}
+ 
+ 
+<?php 
+			}	
+
 	}
 
 
-
 ?>
-
