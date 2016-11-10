3a8dccfdee3c3edf16d2faa77af90a75b89c75d1
jws0071 <jws0071@naver.com>
2016-11-10 17:59:05 +0900

    사용자 UI 변경 완료

--- project/Search_user_view.php
+++ project/Search_user_view.php
@@ -36,91 +36,87 @@ $(window).load(function(e){
 </script>
 
 <div id="page-wrapper">
-
-<div class="container-fluid">
+	<div class="container-fluid">
 
 <!-- Page Heading -->
-<div class="row">
-<div class="col-lg-12">
-	<h1 class="page-header">User</h1>
-</div>
-</div>
-
-
-
-
-<!-- /.row -->
-<div class="row">
-<div class="col-lg-12">
-<div class="panel panel-default">
-	<div class="panel-heading">
-		<h3 class="panel-title"><i class="fa fa-bar-chart-o fa-fw"></i> User List</h3>
-	</div>
-<div class="panel-body">
-	<div class="row">
-		<div class="col-lg-6">
-   			<div class="input-group">
-			<!-- <input id="user_search_text" type="text" class="form-control" placeholder="Search for...">
-			     <span class="input-group-btn">
-        			<button class="btn btn-default" > Enter</button>
-      			</span>	-->
-    		</div>
-    		<!-- /input-group -->
-  		</div>
-  		<!-- /.col-lg-6 -->
-  	</div>
-  		<!-- /.row -->
-  <br />
-  		
+		<div class="row">
+			<div class="col-lg-12">
+				<h1 class="page-header">User</h1>
+			</div>
+		</div>
+		<!-- /.row -->
+		
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<h3 class="panel-title"><i class="fa fa-bar-chart-o fa-fw"></i> User List</h3>
+					</div>
+				<div class="panel-body">
 		<?php 
 
 			$DBControlObject = new DBController();
 			$rows = $DBControlObject->getUserList();
 			$cnt = count($rows);
-
-		?>
-<!-- 
-<div class="table-responsive">
-	<table id="user_list_table" class="table table-bordered table-hover table-striped">
-		<tbody>
- -->
-		<?php 
 				if(count($rows)>0) {
+		?>
+				<div class="row">
+                    <div class="col-lg-6">
+                        <div class="table-responsive">
+                            <table class="table table-bordered table-hover" id="device_list_table">
+    
+                                <tbody>
+				<?php
 					for($i=0; $i<count($rows); $i++) {
 						$user_name = ICONV("EUC-KR","UTF-8",$rows[$i][1]);
-			?>
-	 		<!--<td align = "center"></td> -->
-				<button class="btn btn-default" id="user_search_text" value="<?php echo $user_name?>"><?php echo $user_name?></button>
-			<?php
-					}
-				}
-			?>
-<!-- 			
-		</tbody>
-	</table>
-</div>
--->
+				?>
+                                    <tr>
+
+                                        <td><button  class="btn btn-lg btn-primary" value="<?php echo $user_name?>"> <?php echo $user_name?></button></td>
+
+                                    </tr>
+					<?php	
+							}
+							
+					?>	
+						      </tbody>
+                            </table>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="table-responsive">
+                            <table class="table table-bordered table-hover table-striped">
+								<thead>
+									<tr>
+									</tr>
+								</thead>
+                                <tbody>
 	
-	<br>
-	<div ><br> </div>  
-	<div id="div1" class="active" ><br> </div>  		
-		<!-- /table-responsive -->
+                                    <tr>
+										<div id="div1" ><br> </div>  	
+                                    </tr>
+				
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+
+					<?php	
+							}
+							
+					?>	
+					 
+					</div>
+					<!-- /.panel-body -->
+						
+				</div>
+				<!-- /.panel-default -->
+			</div>
+			<!-- /.col-lg-12 -->
+		</div>
+		<!-- /.row -->
+
 	</div>
-	<!-- /.panel-body -->
-</div>
-<!-- /.panel-default -->
-</div>
-<!-- /.col-lg-12 -->
-</div>
-<!-- /.row -->
-
-
-</div>
-<!-- /.container-fluid -->
+	<!-- /.container-fluid -->
 </div>
 <!-- /#page-wrapper -->
-
-
-
-
-
