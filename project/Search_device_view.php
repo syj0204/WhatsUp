db94ea47275ae20e6658882876d593b04f525efb
jws0071 <jws0071@naver.com>
2016-11-10 14:07:27 +0900

    Device search 기능 추가완료

--- project/Search_device_view.php
+++ project/Search_device_view.php
@@ -1,134 +1,85 @@
 <?php
-	include "DBController.php";
-?>
-<!-- 
-<script type="text/javascript">
+	include "DBController.php";?>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js">
+</script>
+<script>
+$(document).ready(function(){
+	$("button").click(function(){
+		var value = $(this).attr("value"); //버튼에 잇는 value 값을 호출해서 변수에 저장
+		alert(value)
+		$.post("Search_device.php",{ 
+			//name: new_user_name
+			name: value
+		}, 
+			function(data,status){
+				if(data==0){
+					alert("No User");
+					$("div#div1").text("No User");  // 데이터가 없는 경우를 나타낸다.
 
-	$(function(){
-		$('#device_search').click(function(){
-			//alert(''+$('#device_search_text').val());
-			var value = $('#device_search_text').val(); 
+				} // 오류시 수정 예정
+				else {
+					//alert(status);
+					$("div#div1").html(data);  //데이터 호출 성공
 
-			$("#device_list_table tbody tr").each(function(){
-				$row = $(this);
-				var text = $row.find("td:eq(2)").text();
-				alert(text);
-				/*if(text.toLowerCase()==value.toLowerCase()) {
-					
-				}*/
-				
-			});
-		});
-
+				}
+			});	
 	});
 
-	$(window).load(function(e){
+}); // ajax 방식을 통해서 PHP에 POST 형식으로 데이터 값을 넘기고 다시 콜백으로 값을 받아서 출력함
+$(window).load(function(e){
 
-	});
+});	// 별 쓸모가 없는 코드....
 
 </script>
- -->
+
 <div id="page-wrapper">
 
-<div class="container-fluid">
-
-<!-- Page Heading -->
-<div class="row">
-<div class="col-lg-12">
-	<h1 class="page-header">Device</h1>
-</div>
-</div>
-
-<!-- /.row -->
-<div class="row">
-<div class="col-lg-12">
-<div class="panel panel-default">
-	<div class="panel-heading">
-		<h3 class="panel-title"><i class="fa fa-bar-chart-o fa-fw"></i> Device List</h3>
-	</div>
-	<div class="panel-body">
+	<div class="container-fluid">
+		<!-- Page Heading -->
 		<div class="row">
-		<div class="col-lg-6">
-   		<!-- <div class="input-group">
-      			<input id="device_search_text" type="text" class="form-control" placeholder="Search for...">
-     			<span class="input-group-btn">
-        			<button id="device_search" class="btn btn-default" type="button">Search</button>
-      			</span>
-    		</div>
-    			<button id="add_device" class="btn btn-default" type="button">Add Device</button>
-    	 -->	
-    		<!-- /input-group -->
-  		</div>
-  		<!-- /.col-lg-6 -->
-  	
-  		</div>
-  		<!-- /.row -->
-  		
-  		<br />
-  		
-  		<div class="table-responsive">
-			<table id="device_list_table" class="table table-bordered table-hover table-striped">
-				<thead>
-					<tr>
-						<th>Device ID</th>
-						<th>Device Name</th>
-					</tr>
-				</thead>
-				<tbody>
-				<?php 
+			<div class="col-lg-12">
+				<h1 class="page-header">User</h1>
+			</div>
+		</div>
+
+		<!-- /.row -->
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<h3 class="panel-title"><i class="fa fa-bar-chart-o fa-fw"></i> User List</h3>
+					</div>
+					<div class="panel-body">
+					<?php 
+
 					$DBControlObject = new DBController();
 					$rows = $DBControlObject->getDeviceList();
-					if(count($rows)/2>0) {
-						for($i=0; $i<count($rows)/2; $i++) {
-								$rows1 = ICONV("EUC-KR","UTF-8",$rows[$i][0]);
-								$rows2 = ICONV("EUC-KR","UTF-8",$rows[$i][1]);
-				?> 					
-					<tr>
-						<td><?php echo $rows1?></td>
-						<td><?php echo $rows2?></td>
-					</tr>
-				<?php 
+					$cnt = count($rows);
+						if(count($rows)>0) {
+							for($i=0; $i<count($rows); $i++) {
+								$device_name = ICONV("EUC-KR","UTF-8",$rows[$i][1]);
+					?>
+					<button class="btn btn-default" id="device_search_text" value="<?php echo $device_name?>"><?php echo $device_name?></button>
+					<?php
+							}
 						}
-					}
-				?>
-				</tbody>
-
-				<thead>
-					<tr>
-						<th>Device ID</th>
-						<th>Device Name</th>
-					</tr>
-				</thead>
-				<tbody>
-				<?php
-					if (count($rows)>count($rows)/2+1){
-						for($i=count($rows)/2+1; $i<count($rows); $i++) {
-								$rows3 = ICONV("EUC-KR","UTF-8",$rows[$i][0]);
-								$rows4 = ICONV("EUC-KR","UTF-8",$rows[$i][1]);
-				?> 					
-					<tr>
-						<td><?php echo $rows3?></td>
-						<td><?php echo $rows4?></td>
-					</tr>
-				<?php 
-						}
-					}
-				?>
-				</tbody>
-			</table>
+					?>
+					<div ><br> </div>  
+					<div id="div1" ><br> </div>  		
+					</div>
+					<!-- /.panel-body -->
+				</div>
+				<!-- /.panel-default -->
+			</div>
+			<!-- /.col-lg-12 -->
 		</div>
-		<!-- /table-responsive -->
+		<!-- /.row -->
 	</div>
-	<!-- /.panel-body -->
-</div>
-<!-- /.panel-default -->
-</div>
-<!-- /.col-lg-12 -->
-</div>
-<!-- /.row -->
-
-
-</div>
-<!-- /.container-fluid -->
+	<!-- /.container-fluid -->
 </div>
 <!-- /#page-wrapper -->
+
+
+
+
+
