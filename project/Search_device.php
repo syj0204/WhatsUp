c6adde1585ee18e367f0266e8f00b625a8d41d8f
jws0071 <jws0071@naver.com>
2016-11-10 17:59:05 +0900

    사용자 UI 변경 완료

--- project/Search_device.php
+++ project/Search_device.php
@@ -9,12 +9,23 @@
 	$DBControlObject = new DBController();
 	$rows = $DBControlObject->getDisPlayNameUser($sDisplayName);  // 퀴리문 호출하여서 값을 받음
 	if(count($rows)>0) {
-			echo "User List : ";
 		for($i=0; $i<count($rows); $i++) {  // 레코드셋을 통재로 가져오기 때문에 배열로 나타내야함
 			$device_name = ICONV("EUC-KR","UTF-8",$rows[$i][1]);
 
-			echo $device_name;
-			echo " ";
+?>
+
+<div class="col-lg-6">
+       <div class="table-responsive">
+          <table class="table table-bordered table-hover">        
+             <tbody>
+               <tr > <?php echo $device_name; ?></tr>
+              </tbody>
+          </table>
+     </div>
+  </div>
+ 
+ 
+<?php 
 			}	
 
 	}
