daf881a96a582bbc62c0fb87d8dcd03d5ad0b341
suh.youngjoo <suh.youngjoo@suhyoungjoo-PC>
2016-11-14 14:54:20 +0900

    잘하겠스무니다ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ

---
+++ project/category.php
@@ -0,0 +1,35 @@
+
+<?php
+include "DBController.php";
+
+
+$category = $_POST["category"];
+
+$DBControlObject = new DBController();
+$result = null;
+
+switch($category) {
+	case 'user':
+		$result = $DBControlObject->getUserList();
+		break;
+	case 'device':
+		$result = $DBControlObject->getDeviceList();
+		break;
+	case 'devicegroup':
+		$result = $DBControlObject->getDeviceGroupList();
+		break;
+	case 'host':
+		//$result = $DBControlObject->;
+		break;
+		
+}
+//echo json_encode($result);
+$new_result="";
+for($i=0; $i<count($result); $i++) {
+	for($j=0; $j<count($result[$i]); $j++) {
+		$new_result = $new_result.",".ICONV("EUC-KR","UTF-8",$result[$i][$j]);
+	}
+	$new_result = $new_result."/";
+}
+echo $new_result;
+?>
