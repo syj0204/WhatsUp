06502a4330c54e37113822676d343e2312980217
jws0071 <jws0071@naver.com>
2016-11-15 14:32:49 +0900

    permission 추가 내용 부분 현재내용까지 결합 완료

--- project/test_post.php
+++ project/test_post.php
@@ -47,6 +47,112 @@
 			);
 		});
 	});
+	$('#edit_permission').click(function(){
+
+		$('#list3').attr( "size", 11 );
+		$('#list4').show();
+		//$('#edit_permission').text("SAVE");
+		$('#edit_permission').hide();
+		$('#edit_permission_save').show();
+		$('#edit_permission_cancel').show();
+
+		/*var list3_array = [];
+		$('#list3 option').each(function() {
+			list3_array.push($(this));
+		});
+
+		$('#list3 option').remove();
+		for(var i=3; i<list3_array.length; i++) {
+			$('#list3').append("<option value="+list3_array[i].val()+">"+list3_array[i].text()+"</option>");
+		}*/
+
+		var selected_category = $('#list1 option:selected').val();
+		var selected_item = $('#list2 option:selected').val();
+
+		$.post("get_no_permission_device_list.php",{
+			category:selected_category,
+			item:selected_item
+			}, 
+			function(data,status) {
+				//alert(data);
+				var data_by_category_item = data.split('/');
+				//alert(data_by_category.length);
+				for(var i=0; i<data_by_category_item.length-1; i++) {
+					//alert(data_by_category[i]);
+					var value = data_by_category_item[i].split(',');
+					//alert(value.length);
+					$('#list4').append("<option value="+value[1]+">"+value[1]+"."+value[2]+"</option>");
+				}
+			}
+		);
+		/*var $dialog_select = $('<select>');
+        $text.append('<option>temp</option>');
+        $text.append('</select>');
+        
+        BootstrapDialog.show({
+            title: 'Edit Permission',
+            message: $text,
+            buttons: [{
+                label: 'Save',
+                action: function(dialogRef){
+                    dialogRef.close();
+                }
+            }, {
+                label: 'cancel',
+                action: function(dialogRef){
+                    dialogRef.close();
+                }
+            }]
+        });*/
+	});
+
+	$('#list3').change(function(){
+		var to_delete_item = $('#list3 option:selected').remove().appendTo('#list4').val();
+		to_delete_list.push(to_delete_item);
+	});
+
+	$('#list4').change(function(){
+		var to_add_item = $('#list4 option:selected').remove().appendTo('#list3').val();
+		//new_added_lists.push(new_list_item);
+		to_add_list.push(to_add_item);
+	});
+
+	$('#edit_permission_save').click(function(){
+		var selected_category = $('#list1 option:selected').val();
+		var selected_item = $('#list2 option:selected').val();
+
+		alert(selected_category);
+		alert(selected_item);
+
+		switch(selected_category) {
+			case 'user':
+				//alert("user");
+				//alert(to_add_list);
+				$.post("permission_update.php",{
+					user:selected_item,
+					devicearray:to_add_list
+					}, 
+					function(data,status) {
+						alert(data);
+					}
+				);
+				break;
+			case 'device':
+				break;
+			case 'devicegroup':
+				break;
+			case 'host':
+				//$result = $DBControlObject->;
+				break;
+		}
+		
+		$('#list4').hide();
+		$('#list3').attr( "size", 25 );
+		$('#edit_permission').show();
+		$('#edit_permission_save').hide();
+		$('#edit_permission_cancel').hide();
+		
+	});
 
 </script>
 	
@@ -109,9 +215,19 @@
 			</div>
 			<!-- /.col-xs-3 -->			
 
-			<div class="col-xs-3">
+			
+			<div class="col-xs-2">
 				<select name="list3" id="list3" class="form-control" size="25">
 				</select>
+				<br />
+				<select name="list4" id="list4" class="form-control" size="11" style="display: none">
+				</select>
+			</div>
+			<!-- /.col-xs-2 -->	
+			<div class="col-xs-3" align="center">
+				<button id="edit_permission" class="btn btn-default" type="button">Edit Permission</button>
+				<button id="edit_permission_save" class="btn btn-default" type="button" style="display: none">Save</button>
+				<button id="edit_permission_cancel" class="btn btn-default" type="button" style="display: none">Cancel</button>
 			</div>
 		</div>
 		<!-- /.row -->	
