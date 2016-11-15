750ccf3666a45aeff6f78e94dccaf0d669198454
suh.youngjoo <suh.youngjoo@suhyoungjoo-PC>
2016-11-15 16:20:24 +0900

    db 클래스 커밋

--- project/Database.php
+++ project/Database.php
@@ -12,9 +12,9 @@ class Database {
 	
 	function connectDB() {
 		
-		$this->server = "";
+		$this->server = "203.249.128.60";
 		$this->uid = "youngjoo";
-		$this->pwd = "dudwn0514"; 
+		$this->pwd = "$%dudwn0514^*(";
 		$this->database = "whatsup";
 		
 		$this->connectionInfo = array( "UID"=>$this->uid,
