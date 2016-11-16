fadef9c57e11317a2e7b2542febf4c321fc7a91f
jws0071 <jws0071@naver.com>
2016-11-16 09:10:03 +0900

    test2/test.php생성 및 dbCon에 쿼리문 생성

--- project/permission_control_view.php
+++ project/permission_control_view.php
@@ -1,5 +1,5 @@
 <?php
-	include "DBController.php";
+	include "DBController.php"; 
 ?>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 <script type="text/javascript">
