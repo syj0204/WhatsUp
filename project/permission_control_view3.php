dd85bf7e82b556e876469859bcf915ee3685142a
suh.youngjoo <suh.youngjoo@suhyoungjoo-PC>
2016-11-16 18:14:48 +0900

    permission view 전면 수정중

--- project/permission_control_view3.php
+++ project/permission_control_view3.php
@@ -1,6 +1,22 @@
 <?php
-	include "DBController.php"; 
+	include "DBController.php";
+	
+	$han="추가";
+	$han1="수정";
+	$han2="삭제";
+	$han3="검색";
+	$han4="취소";
+	$han5="완료";
+	$han6="이름으로 찾기";
+	$han = ICONV("EUC-KR","UTF-8",$han);
+	$han1 = ICONV("EUC-KR","UTF-8",$han1);
+	$han2 = ICONV("EUC-KR","UTF-8",$han2);
+	$han3 = ICONV("EUC-KR","UTF-8",$han3);
+	$han4 = ICONV("EUC-KR","UTF-8",$han4);
+	$han5 = ICONV("EUC-KR","UTF-8",$han5);
+	$han6 = ICONV("EUC-KR","UTF-8",$han6);
 ?>
+
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 <script type="text/javascript">
 
@@ -53,6 +69,7 @@
 		$('#list3 option').remove();
 		$('#list2_title').text("List By Category");
 		$('#list3_title').text("Permission List");
+		$('#permission_list_table tr').remove();
 	}
 
 	$(function(){
@@ -75,7 +92,41 @@
 		});
 
 		$('#list2').change(function(){
-			$('#list3 option').remove();
+			$('#permission_list_table tr').remove();
+			$('#permission_list_table').show();
+			var list1_item = $('#list1 option:selected').val();
+			var list2_item = $('#list2 option:selected').val();
+			switch(list1_item) {
+			case 'user':
+				$('#permission_list_table').append('<tr><th>Device ID</th><th>Device Group</th><th>Device Name</th><th>Option</th></tr>');
+				break;
+			default:
+				$('#permission_list_table').append('<tr><th>Department</th><th>User Name</th><th>Option</th></tr>');
+				break;
+			}
+
+			$.post("item1.php",{
+				category:list1_item,
+				item:list2_item
+				}, 
+				function(data,status) {
+					var data_by_list1 = data.split('|');
+					switch(list1_item) {
+					case 'user':
+						for(var i=0; i<data_by_list1.length-1; i++) {
+							var value = data_by_list1[i].split(',');
+							//$.newtr = $("<tr><td style='display: none'>"+data+"</td><td>"+new_user_name+"</td><td>"+new_user_cellphone+"</td><td>"+new_user_department+"</td><td><button id='edit_user' class='btn btn-default' type='button' onclick='edit_user(this)'><?php echo $han1?></button>      <button id='delete_user' class='btn btn-default' type='button' onclick='delete_user(this)'><?php echo $han2?></button></td></tr>");
+							$.newtr = $("<tr><td>"+value[1]+"</td><td></td><td>"+value[2]+"</td><td><button id='edit_user' class='btn btn-default' type='button' onclick='edit_user(this)'><?php echo $han1?></button>      <button id='delete_user' class='btn btn-default' type='button' onclick='delete_user(this)'><?php echo $han2?></button></td></tr>");
+							$('#permission_list_table').append($.newtr);
+						}
+						break;
+					default:
+						//$('#permission_list_table').append('<tr><th>Department</th><th>User Name</th><th>Option</th></tr>');
+						break;
+					}
+				}
+			);
+			/*$('#list3 option').remove();
 			$('#list4 option').remove();
 			$('#add_permission').show();
 			$('#delete_permission').show();
@@ -93,7 +144,7 @@
 						$('#list3').append("<option value="+value[1]+">"+value[1]+","+value[2]+"</option>");
 					}
 				}
-			);
+			);*/
 		});
 
 		$('#add_permission').click(function(){
@@ -202,7 +253,7 @@
 	});
 
 </script>
-	
+
 <div id="page-wrapper">
 
 <div class="container-fluid">
@@ -210,8 +261,7 @@
 <!-- Page Heading -->
 <div class="row">
 <div class="col-lg-12">
-	<h1 class="page-header">Permission</h1>
-
+	<h1 class="page-header">User</h1>
 </div>
 </div>
 
@@ -220,13 +270,14 @@
 <div class="col-lg-12">
 <div class="panel panel-default">
 	<div class="panel-heading">
-		<h3 class="panel-title"><i class="fa fa-bar-chart-o fa-fw"></i> Permission Search</h3>
+		<h3 class="panel-title"><i class="fa fa-bar-chart-o fa-fw"></i> Permission List</h3>
 	</div>
 	<div class="panel-body">
-	<div class="row">
+		<div class="row">
 		<div class="form-group">
 			<!-- <label for="list1" class="col-sm-2 control-label">Search Category</label> -->
 			<div class="col-sm-2">
+			<label id="list1_title">Search Category</label>
 	    	<select name="list1" id="list1" class="form-control selcls">
 	    		<option> -- Select Search Category -- </option>
 				<option value='user'>User</option>
@@ -256,51 +307,75 @@
 	    		</select>
 			</div>
 		</div>
-	</div>
-	<br />
+		</div>
+		<br />
 	
-	<div class="row">
-		<div class="form-group">
-			<!-- <label for="list1" class="col-sm-2 control-label">Search Category</label> -->
-			<div class="col-xs-2">
-				<label id="list2_title">List By Category</label>
-	    		<select name="list2" id="list2" class="form-control selcls">
-	    		</select>
-			</div>
-			<div class="col-xs-2">
-				<label id="list3_title">Permission List</label>
-	    		<select name="list3" id="list3" class="form-control selcls" size="20">
-	    		</select>
-			</div>
-			<div class="col-xs-2">
-				<label id="list4_title" style="display: none">Available Options To Add</label>
-	    		<select name="list4" id="list4" class="form-control selcls" size="20" style="display: none" multiple="multiple">
-	    		</select>
-			</div>
-			<div class="col-xs-3" align="center">
-				<button id="add_permission" class="btn btn-default" type="button" style="display: none" >Add Permission</button> <br /><br />
-				<button id="delete_permission" class="btn btn-default" type="button" style="display: none" >Delete Permission</button>
-				<button id="save" class="btn btn-default" type="button" style="display: none" disabled="true">Save</button>
-				<button id="cancel" class="btn btn-default" type="button" style="display: none" disabled="true">Cancel</button>
-			</div>
-		</div>
-	</div>
-	<br />			
-			
-		</div>
-		<!-- /.row -->
-	</div>
-	<!-- /.row -->
+		<div class="row">
+		<div class="col-lg-6">
+   			<div class="input-group">
+      			<input id="user_search_text" type="text" class="form-control" placeholder="<?php echo $han6?>~~">
+     			<span class="input-group-btn">
+        			<button id="search_user" class="btn btn-default" type="button"><?php echo $han3?></button>
+      			</span>
+    		</div>
+    		<!-- /input-group -->
+  		</div>
+  		<!-- /.col-lg-6 -->
+  		<button id="add_user" class="btn btn-default" type="button"><?php echo $han?></button>
+  		</div>
+  		<!-- /.row -->
+  		<br />
+  		<div id="permission_table_view">You can check permission! Just Check Each Category Items!!
+  			<table id="permission_list_table" class="table table-bordered table-hover table-striped" style="display: none">
+  			</table>
+  		</div>
+ 
+  		<!-- <div class="table-responsive">
+			<table id="permission_list_table" class="table table-bordered table-hover table-striped" style="display: none">
+				<thead>
+					<tr>
+						<th style="display: none">User ID</th>
+						<th>User Name</th>
+						<th>Cell Phone</th>
+						<th>Department</th>
+						<th>Option</th>
+					</tr>
+				</thead>
+				<tbody>
+
+				<?php 
+					$DBControlObject = new DBController();
+					$rows = $DBControlObject->getUserList();
+					if(count($rows)>0) {
+						for($i=0; $i<count($rows); $i++) {
+							$user_name = ICONV("EUC-KR","UTF-8",$rows[$i][1]);
+				?>
+					<tr>
+						<td style="display: none"><?php echo $rows[$i][0]?></td>
+						<td><?php echo $user_name?></td>
+						<td><?php echo $rows[$i][2]?></td>
+						<td><?php echo $rows[$i][3]?></td>
+						<td><button id="edit_user" class="btn btn-default" type="button" onclick="edit_user(this)"><?php echo $han1?></button>      <button id="delete_user" class="btn btn-default" type="button" onclick="delete_user(this)"><?php echo $han2?></button></td>
+					</tr>
+				<?php
+						}
+					}
+				?>
+				</tbody>
+			</table>
+		</div> -->
+		<!-- /table-responsive -->
 	</div>
 	<!-- /.panel-body -->
 </div>
-<!-- /.panel panel-default -->
+<!-- /.panel-default -->
 </div>
 <!-- /.col-lg-12 -->
 </div>
 <!-- /.row -->
+
+
 </div>
 <!-- /.container-fluid -->
 </div>
-<!-- /.page-wrapper -->				
-				
+<!-- /#page-wrapper -->
