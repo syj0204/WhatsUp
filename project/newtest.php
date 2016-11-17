75893fe5fa1190498586e6fd2baeba18f8f301ea
jws0071 <jws0071@naver.com>
2016-11-17 15:24:11 +0900

    중복 방지 및 전체적은 UI를 수정함

--- project/newtest.php
+++ project/newtest.php
@@ -4,10 +4,12 @@
 	$han = ICONV("EUC-KR","UTF-8",$han);
 	$han1="선택한 템플릿에 포함된 디바이스 정보";
 	$han1 = ICONV("EUC-KR","UTF-8",$han1);
-	$han2="사용자 명단";
+	$han2="사용자 선택(다중선택 Ctrl+클릭)";
 	$han2 = ICONV("EUC-KR","UTF-8",$han2);
 	$han3="템블릿 선택";
 	$han3 = ICONV("EUC-KR","UTF-8",$han3);
+	$han4="템블릿 적용";
+	$han4 = ICONV("EUC-KR","UTF-8",$han4);
 	
 ?>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
@@ -68,10 +70,8 @@ $(function(){
 <!-- Page Heading -->
 <div class="row">
 <div class="col-lg-12">
-	<h1 class="page-header">Select</h1>
-	<ol class="breadcrumb">
-		<li class="active"><i class="fa fa-dashboard"></i>Select</li>
-	</ol>
+	<h1 class="page-header">Match</h1>
+
 </div>
 </div>
 
@@ -80,7 +80,7 @@ $(function(){
 <div class="col-lg-12">
 <div class="panel panel-default">
 	<div class="panel-heading">
-		<h3 class="panel-title"><i class="fa fa-bar-chart-o fa-fw"></i>Select Template</h3>
+		<h3 class="panel-title"><i class="fa fa-bar-chart-o fa-fw"></i>Match User/Template</h3>
 	</div>
 	<div class="panel-body">
 	<!-- <div id="morris-area-chart"></div> -->
@@ -92,7 +92,7 @@ $(function(){
     	<div class="row">
         	<div class="col-xs-6">
         		<label id="list1_title"><?php echo $han2?></label>
-        		<select name="list1" id="list1" class="form-control" size="25" multiple="multiple" >
+        		<select name="list1" id="list1" class="form-control " size="25" multiple="multiple" >
 						<?php $DBControlObject = new DBController();
 							  $rows = $DBControlObject->getUserList();
 								if(count($rows)>0) {		
@@ -111,35 +111,33 @@ $(function(){
         		</div>
 			<!-- /.col-xs-4 -->			
 
-			<div class="col-xs-6">
-				<div class="col-xs-8">
-			   <label id="list10_title"><?php echo $han3?></label>
-			   <select name="list10" id="list10" class="form-control">
-        			<option>--Select Template --</option>
-						<?php $DBControlObject = new DBController();
-							  $rows = $DBControlObject->getSelecttemp();// 초기 Template select문에 나타내는 것
-								if(count($rows)>0) {		
-									for($i=0; $i<count($rows); $i++) {
-									$device_name = ICONV("EUC-KR","UTF-8",$rows[$i][1]);
-						?>
-					<option value=<?php echo $rows[$i][0]?>>      		 		
-						<?php
-										echo "Template / " .$device_name." <br> \n";
-									}
-								}8
-						?>
-					</option>
-			   </select>
-			   </div>
-			   <div class="col-xs-3"><br>
-			   <button id="enter"  >enter</button>
-			   </div>
+			<div class="col-xs-6"> 
+				<label id="list10_title"><?php echo $han3?></label>
+					<div class="form-group input-group">
+					   <select name="list10" id="list10" class="form-control panel-yellow"">
+		        			<option>--Select Template --</option>
+								<?php $DBControlObject = new DBController();
+									  $rows = $DBControlObject->getSelecttemp();// 초기 Template select문에 나타내는 것
+										if(count($rows)>0) {		
+											for($i=0; $i<count($rows); $i++) {
+											$device_name = ICONV("EUC-KR","UTF-8",$rows[$i][1]);
+								?>
+							<option value=<?php echo $rows[$i][0]?>>      		 		
+								<?php
+												echo "Template / " .$device_name." <br> \n";
+											}
+										}
+								?>
+							</option>
+					   </select>
+					   <span class="input-group-btn"><button id="enter" class="btn btn-success" type="button"><?php echo $han4?></button></span>
+					 </div>
 			</div>
 			<div class="col-xs-6">
 				
 				
 				<label id="list20_title"><?php echo $han1?></label>
-				<select name="list20" id="list20" class="form-control" size="25"></select>
+				<select name="list20" id="list20" class="form-control" size="23.5"></select>
 				
 				
 			</div>
