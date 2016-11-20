e971a5a397e45bc11247cd329f5996e5cb7613f4
suh.youngjoo <suh.youngjoo@suhyoungjoo-PC>
2016-11-20 16:01:43 +0900

    user_delete 오류 수정

--- project/delete_user.php
+++ project/delete_user.php
@@ -2,11 +2,10 @@
 include "DBController.php";
 
 $user_id =$_POST["userid"];
-$user_id =intval($user_id);
-//$DBControlObject = new DBController();
-//$result = $DBControlObject->deleteUser($user_id);
+$DBControlObject = new DBController();
+$result = $DBControlObject->deleteUser($user_id);
 
-
-echo $user_id;
+if($result) echo "success";
+else echo "fail!!";
 ?>
 
