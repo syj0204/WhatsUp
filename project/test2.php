54017807d89b057646386013e80074b942b4a06c
jws0071 <jws0071@naver.com>
2016-12-05 11:18:17 +0900

    템플릿 이름 넣는 inputbox SQL인젝션 방지

--- project/test2.php
+++ project/test2.php
@@ -1,25 +1,26 @@
 
 <?php
 include "DBController.php";
+include "SQL_injection.php";
 $han="템플릿 저장완료";
 $han = ICONV("EUC-KR","UTF-8",$han);
 $han1="템플릿 저장실패(이름 중복을 확인하세요~)";
 $han1 = ICONV("EUC-KR","UTF-8",$han1);
 //한글화 하기 위한 작업
 
-$temp_size= $_POST["size"];
+$temp_size=$_POST["size"];
 $temp_name= $_POST["name"];
 $temp_name = ICONV("UTF-8","EUC-KR",$temp_name);
 // test.php에서 값을 전달 받아 변수 저장
-
+$temp_name=SQL_injection($temp_name);
 $temp_string=$_POST["category"];
+
 $temp_string = substr($temp_string , 0, -1);
 $result_first = explode(",", $temp_string);
 sort($result_first);
 $result_first= implode(",", $result_first);
 $result_first = $result_first.",";
 //전달 받은 값을 정령시키고 다시 문자열로 변경함
-
 
 $DBControlObject = new DBController();
 $rows= null;
